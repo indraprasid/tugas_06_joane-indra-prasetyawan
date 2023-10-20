a749a9b7de25ea780b0ba2fb705d8ea6bb27351f
Joane Indra Prasetyawan <joaneindra.prasetyawan@mitrais.com>
2023-10-20 23:13:16 +0700

    Commit

--- saucedemo-automation/src/test/java/cucumber/stepDef/login.java
+++ saucedemo-automation/src/test/java/cucumber/stepDef/login.java
@@ -1,9 +1,13 @@
 package cucumber.stepDef;
 
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
+
+/*
 import io.cucumber.java.en.When;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+*/
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,72 +16,69 @@ public class login {
     @Given("User already open the sauce demo site.")
     public void userAlreadyOpenTheSauceDemoSite() {
 
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com");
     }
 
-    @When("User enters the valid username, i.e. standard_user")
-    public void userEntersTheValidUsernameIEStandard_user() {
-        throw new io.cucumber.java.PendingException();
-    }
+/*
+ @When("User enters the valid username, i.e. standard_user")
+ public void userEntersTheValidUsernameIEStandard_user() {
 
-    @And("User enters the valid password, i.e. secret_sauce")
-    public void userEntersTheValidPasswordIESecret_sauce() {
-        throw new io.cucumber.java.PendingException();
-    }
+     System.out.println("Hello world!");
+ }
 
-    @And("User clicks the Login button")
-    public void userClicksTheLoginButton() {
-        throw new io.cucumber.java.PendingException();
-    }
+ @And("User enters the valid password, i.e. secret_sauce")
+ public void userEntersTheValidPasswordIESecret_sauce() {
 
-    @Then("The user is logged in successfully")
-    public void theUserIsLoggedInSuccessfully() {
-        throw new io.cucumber.java.PendingException();
-    }
+     System.out.println("Hello world!");
+ }
 
-    @And("Redirected to the Products page")
-    public void redirectedToTheProductsPage() {
-        throw new io.cucumber.java.PendingException();
-    }
+ @And("User clicks the Login button")
+ public void userClicksTheLoginButton() {
+
+     System.out.println("Hello world!");
+ }
+
+ @Then("The user is logged in successfully")
+ public void theUserIsLoggedInSuccessfully() {
+
+     System.out.println("Hello world!");
+ }
+
+ @And("Redirected to the Products page")
+ public void redirectedToTheProductsPage() {
+
+     System.out.println("Hello world!");
+ }
+*/
 
     @Given("User already open the sauce demo site")
     public void userAlreadyOpenTheSauceDemoSites() {
 
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com");
     }
+/*
 
     @And("User enters the valid password, i.e. secret_saucedemo")
     public void userEntersTheValidPasswordIESecret_saucedemo() {
-        throw new io.cucumber.java.PendingException();
+
+        System.out.println("Hello world!");
     }
 
     @Then("The system shows an error message {string}")
     public void theSystemShowsAnErrorMessageEpicSadfaceUsernameAndPasswordDoNotMatchAnyUserInThisService() {
-        throw new io.cucumber.java.PendingException();
+
+        System.out.println("Hello world!");
     }
 
     @And("The user is unsuccessfully logged in")
     public void theUserIsUnsuccessfullyLoggedIn() {
-        throw new io.cucumber.java.PendingException();
-    }
 
-    @Given("B")
-    public void b() {
+        System.out.println("Hello world!");
     }
-
-    @When("C")
-    public void c() {
-    }
-
-    @And("D")
-    public void d() {
-    }
-
-    @Then("E")
-    public void e() {
-    }
+*/
 }
--- saucedemo-automation/src/test/resources/features/login.feature
+++ saucedemo-automation/src/test/resources/features/login.feature
@@ -2,16 +2,16 @@ Feature: Login page
 
   Scenario: Verify the users are able to login with valid credential
     Given User already open the sauce demo site.
-    When User enters the valid username, i.e. standard_user
-    And User enters the valid password, i.e. secret_sauce
-    And User clicks the Login button
-    Then The user is logged in successfully
-    And Redirected to the Products page
+#    When User enters the valid username, i.e. standard_user
+#    And User enters the valid password, i.e. secret_sauce
+#    And User clicks the Login button
+#    Then The user is logged in successfully
+#    And Redirected to the Products page
 
   Scenario: Verify the users are not able to login with invalid credentials
     Given User already open the sauce demo site
-    When User enters the valid username, i.e. standard_user
-    And User enters the valid password, i.e. secret_saucedemo
-    And User clicks the Login button
-    Then The system shows an error message 'Epic sadface: Username and password do not match any user in this service'
-    And The user is unsuccessfully logged in
+#    When User enters the valid username, i.e. standard_user
+#    And User enters the valid password, i.e. secret_saucedemo
+#    And User clicks the Login button
+#    Then The system shows an error message 'Epic sadface: Username and password do not match any user in this service'
+#    And The user is unsuccessfully logged in
