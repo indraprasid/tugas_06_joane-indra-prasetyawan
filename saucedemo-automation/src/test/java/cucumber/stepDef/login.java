31ddafc57224370363125083001ed17deb8073d9
Joane Indra Prasetyawan <joaneindra.prasetyawan@mitrais.com>
2023-10-20 20:14:46 +0700

    Commit

--- saucedemo-automation/src/test/java/cucumber/stepDef/login.java
+++ saucedemo-automation/src/test/java/cucumber/stepDef/login.java
@@ -15,42 +15,52 @@ String baseURL = "https://www.saucedemo.com/";
 
     @Given("User already open the sauce demo site.")
     public void userAlreadyOpenTheSauceDemoSite() {
+        throw new io.cucumber.java.PendingException();
     }
 
     @When("User enters the valid username, i.e. standard_user")
     public void userEntersTheValidUsernameIEStandard_user() {
+        throw new io.cucumber.java.PendingException();
     }
 
     @And("User enters the valid password, i.e. secret_sauce")
     public void userEntersTheValidPasswordIESecret_sauce() {
+        throw new io.cucumber.java.PendingException();
     }
 
     @And("User clicks the Login button")
     public void userClicksTheLoginButton() {
+        throw new io.cucumber.java.PendingException();
     }
 
     @Then("The user is logged in successfully")
     public void theUserIsLoggedInSuccessfully() {
+        throw new io.cucumber.java.PendingException();
     }
 
     @And("Redirected to the Products page")
     public void redirectedToTheProductsPage() {
+        throw new io.cucumber.java.PendingException();
     }
 
     @Given("User already open the sauce demo site")
     public void userAlreadyOpenTheSauceDemoSites() {
+        throw new io.cucumber.java.PendingException();
     }
 
     @And("User enters the valid password, i.e. secret_saucedemo")
     public void userEntersTheValidPasswordIESecret_saucedemo() {
+        throw new io.cucumber.java.PendingException();
     }
 
     @Then("The system shows an error message {string}")
     public void theSystemShowsAnErrorMessageEpicSadfaceUsernameAndPasswordDoNotMatchAnyUserInThisService() {
+        throw new io.cucumber.java.PendingException();
     }
 
     @And("The user is unsuccessfully logged in")
     public void theUserIsUnsuccessfullyLoggedIn() {
+        throw new io.cucumber.java.PendingException();
     }
 
     @Given("B")
