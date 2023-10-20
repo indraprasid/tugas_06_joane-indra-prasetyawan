9a744306288137e18e0ec78bb8afe12c53f3a13e
Joane Indra Prasetyawan <joaneindra.prasetyawan@mitrais.com>
2023-10-20 21:39:37 +0700

    Commit

--- saucedemo-automation/src/test/java/cucumber/runner/TestRunner.java
+++ saucedemo-automation/src/test/java/cucumber/runner/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "cucumber.stepDef",
-        plugin = {"pretty", "html:target/cucumber-reports"},
+        plugin = {"pretty", "html:target/cucumber-reports", "progress"},
         monochrome = true
 )
 public class TestRunner {
